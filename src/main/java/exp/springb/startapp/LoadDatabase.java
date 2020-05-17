58451e1cfcf4344e18c632aafbcdf5038af17474
atfuture7 <to.future7@gmail.com>
2020-05-17 00:12:40 -0700

    add preload, modify for lombok

---
+++ src/main/java/exp/springb/startapp/LoadDatabase.java
@@ -0,0 +1,31 @@
+package exp.springb.startapp;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Configuration
+@Slf4j
+class LoadDatabase {
+	@Bean
+	CommandLineRunner initDatabase(PageRepository repository) {
+		//create simple page
+		Page oPage = new Page("PreTitle1", "created by preload", null);
+		
+		// create new page with list
+		List<String> lstUrl = new ArrayList<String>();
+		lstUrl.add("https://spring.io/guides/tutorials/rest/");
+		lstUrl.add("https://spring.io/guides");
+		Page oPage2 = new Page("PreTitle2", "with list", lstUrl);
+
+		return args-> {
+			log.info("Preloading " + repository.save(oPage));
+			log.info("Preloading " + repository.save(oPage2));
+		};
+	}
+}
--- src/main/java/exp/springb/startapp/PageRepository.java
+++ src/main/java/exp/springb/startapp/PageRepository.java
@@ -3,20 +3,21 @@ package exp.springb.startapp;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
+//import org.springframework.data.repository.query.Param;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+//import org.springframework.data.rest.core.annotation.RestResource;
 
 
-@RepositoryRestResource(collectionResourceRel = "pageurl", path = "pageurl" )
+//@RepositoryRestResource(collectionResourceRel = "pageurl", path = "pageurl" )
 public interface PageRepository extends MongoRepository<Page, String>
 		, PageRepositoryCustom {
 	
-	@RestResource(exported = true)
-	List<Page> findByTitle( String title);
-	@RestResource(exported = true)
+//	@RestResource(exported = true)
+//	List<Page> findByTitle( String title);
+//	@RestResource(exported = false)
+//	List<Page> findByBrief( String str);
+
+	//custom method
 	List<String> findByNotAtt(  String title);
-	@RestResource(exported = false)
-	List<Page> findByBrief( String str);
 }
 
