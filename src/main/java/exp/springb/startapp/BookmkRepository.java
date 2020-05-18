51f5ae5dd5fc8e1f14df341b72c58f8264635275
atfuture7 <to.future7@gmail.com>
2020-05-17 18:48:45 -0700

    check2

---
+++ src/main/java/exp/springb/startapp/BookmkRepository.java
@@ -0,0 +1,24 @@
+package exp.springb.startapp;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BookmkRepository extends MongoRepository<Bookmk, String>
+		, BookmkRepositoryCustom {
+	
+	Bookmk save(Bookmk nBookmk);
+	Optional<Bookmk> findById(String id);
+	List<Bookmk> findByBooknark( String Bookmk);
+	void deleteById( String id);
+	
+	
+	List<Bookmk> findByBookmkLike(String name);
+
+	//custom method
+	Bookmk appendPage(String id, String pid);
+	Bookmk removePage(String id, String pid);
+	Bookmk removePage(String id, int idx);
+}
+
